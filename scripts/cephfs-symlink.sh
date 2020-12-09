1ffb380c26050115ef7ef543daa3d51b19a89a97
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2020-12-09 23:29:15 +0800

    set -euo pipefail

--- scripts/cephfs-symlink.sh
+++ scripts/cephfs-symlink.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -o xtrace
+set -euo pipefail
 
 kubectl get pv -o name | while read line
 do
